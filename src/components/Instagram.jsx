ab0aa2b2a023b2ceb002c73488aa9b0a1004794b
avs-codes <silva892@newschool.edu>
2023-08-31 17:00:00 -0400

    Potential Widget Solution + Mobile CSS Enhancement

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -2,10 +2,20 @@ import { useEffect } from "react";
 
 export default function Instagram() {
   useEffect(() => {
-    if (window.beholdWidgets) {
-    window.beholdWidgets.initialize();
+    try {
+      const widgetJS = document.createElement('script'); 
+      widgetJS.src = 'https://w.behold.so/widget.js';
+      widgetJS.async = true;
+      widgetJS.onload = () => {
+        window.beholdWidgets.initialize();
+      };
+      document.head.appendChild(widgetJS)
+    } catch (error) {
+      console.error('Error loading or initializing the widget:', error)
     }
   }, []);
 
-  return <figure data-behold-id="6woKiegGNe2C7WJWMyUa"></figure>;
+  return ( 
+  <figure data-behold-id="6woKiegGNe2C7WJWMyUa"></figure>
+  );
 }
