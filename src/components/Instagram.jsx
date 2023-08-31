586179b992e35fa685838919f83ed4c1152138bb
avs-codes <silva892@newschool.edu>
2023-08-31 16:01:06 -0400

    Troubleshooting Insta Widget (1)

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 export default function Instagram() {
+  useEffect(() => {
+    if (window.beholdWidgets) {
+    window.beholdWidgets.initialize();
+    }
+  }, []);
 
   return <figure data-behold-id="6woKiegGNe2C7WJWMyUa"></figure>;
 }
