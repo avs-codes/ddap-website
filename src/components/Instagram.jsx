43d243ed15bae8cd89d4a835e902e22e6f188f60
avs-codes <silva892@newschool.edu>
2023-08-31 15:52:24 -0400

    Removed Insta Widget re-initialization

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,9 +1,4 @@
-import { useEffect } from "react";
-
 export default function Instagram() {
-  useEffect(() => {
-    window.beholdWidgets.initialize();
-  }, []);
 
   return <figure data-behold-id="6woKiegGNe2C7WJWMyUa"></figure>;
 }
